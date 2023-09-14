3cf3402a804776966d1e9cdc6ac363b5caff945f
Andrey Emelianov <621077@mail.ru>
2023-09-14 21:15:31 +0300

    начал стилизацию второй секции info

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-$(function () {});
+$(function () {
+  $('.info-tabs__item').on('click', function (e) {
+    e.preventDefault();
+
+    $('.info-tabs__item').removeClass('info-tabs__item--active');
+  });
+});
